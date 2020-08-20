dc5ff456c74637bdd4f918d435c873a1dc568ecd
anette87 <anettem.rivera@gmail.com>
2020-08-19 18:20:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(sourceArr, func){
+    const newArr = []
+    for(let element of sourceArr){
+        newArr.push(func(element))
+    }
+    return newArr
+};
+
+function reduce(sourceArr, func, initialValue = null ){
+    if (initialValue){
+    let accum = initialValue;
+    for(let element of sourceArr){
+        accum = func(element, accum);
+    }
+    return accum
+
+    } else {
+        let accum = sourceArr[0];
+        for(let i = 1; i<sourceArr.length; i++){
+        accum = func(sourceArr[i], accum);
+         }
+         return accum
+    }
+};l
